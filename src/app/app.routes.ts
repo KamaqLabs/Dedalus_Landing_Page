72776a1e184b856474c9542b071db01cb1c72b0f
MiguelCarpioC <134799325+LordSack@users.noreply.github.com>
2025-09-24 17:45:35 -0500

    feat: add home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		component: LandingComponent
+	},
+	// Puedes agregar más rutas aquí si lo necesitas
+];
