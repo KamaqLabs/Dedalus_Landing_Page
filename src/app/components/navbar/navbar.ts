538f68d7e46c572c74de02036e845e763c2c0dc8
Valentino Cervantes Erequita <104030868+KiwiAmenazante@users.noreply.github.com>
2025-12-01 12:58:33 -0500

    feat: update hero link and change app download file in navbar

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -14,8 +14,8 @@ export class NavbarComponent {
   showMenu = false;
 
   downloadApp(): void {
-    const fileUrl = 'assets/files/Imagen.png'; // cámbialo luego por tu APK
-    const fileName = 'Imagen.png';             // o app-release.apk
+    const fileUrl = 'assets/files/DedalusApp.apk'; // cámbialo luego por tu APK
+    const fileName = 'DedalusApp.apk';             // o app-release.apk
 
     const link = document.createElement('a');
     link.href = fileUrl;
